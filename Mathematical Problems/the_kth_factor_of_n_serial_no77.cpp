1625fb94d00c3c68e622a12d01cf0cfa84a90fb5
AdityaGavali <adityagavali18@gmail.com>
2023-07-03 21:45:51 +0530

    second solution

---
+++ Mathematical Problems/the_kth_factor_of_n_serial_no77.cpp
@@ -0,0 +1,18 @@
+//Problem link : https://leetcode.com/problems/the-kth-factor-of-n/description/
+//Solution : https://leetcode.com/problems/the-kth-factor-of-n/submissions/985442379/
+
+//Code :
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int i = 1;
+        while(i<=n){
+            if(n%i == 0){
+                k--;
+                if(k == 0)return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+};
