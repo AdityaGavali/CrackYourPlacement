055e4ae46d442be4e3b6216ad617fac13b76f0f9
AdityaGavali <adityagavali18@gmail.com>
2023-07-12 12:17:29 +0530

    solution no 4

---
+++ Arrays/move_zeros.cpp
@@ -0,0 +1,19 @@
+//problem link: https://leetcode.com/problems/move-zeroes/
+//solution: 
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int c = 0;
+       for(int i = 0;i<nums.size();i++){
+           if(nums[i] == 0)
+           {
+           c++;
+           nums.erase(nums.begin()+i);
+           }
+       }
+       while(c--){
+           nums.push_back(0);
+       }
+        
+    }
+};
